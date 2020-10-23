0877bb15f10881a80f720c0e96a83e87e5557eaa
yoqeyodo <lirimsul@gmail.com>
2020-10-23 15:54:51 +0200

    14 - Multi Option Button

--- MultiOptionButton/lib/main.dart
+++ MultiOptionButton/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter MultiOption Button',
+      title: 'Flutter Multi Option Button',
       home: MultiOptionButton(),
     );
   }
